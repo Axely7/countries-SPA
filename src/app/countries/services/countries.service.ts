189be1b11c7bc47b6be5423c416df21364186882
Axely7 <axely7@hotmail.com>
2024-07-29 12:48:10 -0600

    using routing and showing country information

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -11,6 +11,14 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(() => of(null))
+      );
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
