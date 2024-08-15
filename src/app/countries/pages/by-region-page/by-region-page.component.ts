d22695ffedc449848bb0050d75b366e47b6ae8bc
Axely7 <axely7@hotmail.com>
2024-08-15 11:13:28 -0600

    persisting information with localstorage in service

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/region.type';
@@ -10,12 +10,18 @@ import { Region } from '../../interfaces/region.type';
   templateUrl: './by-region-page.component.html',
   styles: []
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public regions: Country[] = [];
   public regionsC: Region[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
   public selectedRegion?: Region;
 
+
   constructor(private countriesService: CountriesService){}
+
+  ngOnInit(): void {
+    this.regions = this.countriesService.cacheStore.byRegions.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegions.region;
+  }
 
   searchByRegions(region: Region): void{
     this.selectedRegion = region;
